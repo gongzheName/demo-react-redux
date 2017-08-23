c90e550097340f00168464832d42a377e5da8d11
hezz <gongzhe_mail@163.com>
2017-08-23 09:54:05 +0800

    test1ST

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require("webpack")
+/*var webpack = require("webpack")
 module.exports = {
 	entry: "./src/app.js",
 	output: {
@@ -18,5 +18,43 @@ module.exports = {
 			test: /\.css$/,
 			loader: "style-loader!css-loader"
 		}]
+	}
+}*/
+
+
+
+
+var webpack = require("webpack")
+var optimist = require("optimist")
+var argv = optimist.argv
+console.log("测试")
+console.log(argv)
+var port = argv.port || 8000;
+module.exports = {
+	entry: "./src/app.js",
+	output: {
+		path: __dirname+"/build",
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader",
+			query: {
+				plugins: ["transform-runtime"],
+				presets: ["es2015", "react", "stage-2"]
+			}
+		},{
+			test: /\.css$/,
+			loader: "style-loader!css-loader"
+		}]
+	},
+	devServer:{
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		port: port,
+		stats: { colors: true }
 	}
 }
